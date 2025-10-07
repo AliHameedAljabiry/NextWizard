e23e996ad41b500f55eff211f3b49f6e1872d53f
algapry <alihameed7121996ph@gmail.com>
2025-10-07 19:58:15 +0300

    change middleware 001

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,21 @@
-export { auth as middleware } from "@/auth"
+// middleware.ts
+import { auth } from "@/auth";
+
+export default auth((req) => {
+
+
+  // If user is not authenticated, redirect to sign-in
+  if (!req.auth) {
+    const signInUrl = new URL("/sign-in", req.nextUrl.origin);
+    signInUrl.searchParams.set("callbackUrl", req.url);
+    return Response.redirect(signInUrl);
+  }
+});
+
+// Configure which routes to use middleware on
+export const config = {
+  matcher: [
+   
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
+};
